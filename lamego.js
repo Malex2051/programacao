4f74230059121c8bfcca18b3073965434248785f
Malex2051 <marco2051@hotmail.com>
2016-06-14 16:07:11 +0100

    criei

---
+++ lamego.js
@@ -0,0 +1,22 @@
+$(function() {
+    
+   $(".botao").click(function(){
+    $(".menu").slideToggle("slow");
+       $(".Nomem").css("display","none");
+});
+    $(".thumbs a").click(function() {
+		var path = $(this).attr("href");
+		var alt = $(this).attr("title");
+		
+		$("#imagem").attr({src: path, alt: alt});
+		return false;
+	});	
+
+    $("#scroll").click(function(event){
+                       event.preventDefault();
+$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
+                       
+                       
+                       })
+});
+
